8987fcd4e6c4d55b394f163d36eac3934394b609
davidcibin <ddcibin@gmail.com>
2021-06-03 23:30:31 -0400

    ready to upload to webhost

--- js/main.js
+++ js/main.js
@@ -198,7 +198,7 @@ jQuery(document).ready(function($) {
 		window.onscroll = function() {scrollFunction()};
 
 		function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
 			mybutton.style.display = "block";
 		} else {
 			mybutton.style.display = "none";
