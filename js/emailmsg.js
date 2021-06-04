79fc0c65cb1191c80af98a55088d46d526f8ab2e
davidcibin <ddcibin@gmail.com>
2021-06-03 23:25:04 -0400

    ready to upload to webhost

--- js/emailmsg.js
+++ js/emailmsg.js
@@ -1,8 +1,8 @@
 function msgDiv() {
     if ((window.location.href.split("?", 2)[1]) === "confirm") {
       setTimeout(function () { document.getElementById("confirmation").style.display = "block"; }, 500);
-      setTimeout(function () { $(".show-msg").addClass("active"); }, 3000);      
-      setTimeout(function () { document.getElementById("confirmation").style.display = "none"; }, 4000);
+      setTimeout(function () { $(".show-msg").addClass("active"); }, 5000);      
+      setTimeout(function () { document.getElementById("confirmation").style.display = "none"; }, 6000);
     }
 }
 msgDiv();
--- js/main.js
+++ js/main.js
@@ -189,6 +189,30 @@ jQuery(document).ready(function($) {
 	};	
 	sideDivSlider();
 
+	// scroll to top button
+	var scrollTop = function () {
+		//Get the button
+		var mybutton = document.getElementById("myBtn");
+
+		// When the user scrolls down 20px from the top of the document, show the button
+		window.onscroll = function() {scrollFunction()};
+
+		function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			mybutton.style.display = "block";
+		} else {
+			mybutton.style.display = "none";
+		}
+		}
+
+		// When the user clicks on the button, scroll to the top of the document
+		function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+		}
+	};	
+	scrollTop();
+
 	var siteCarousel = function () {
 		if ( $('.owl-carousel-2').length > 0 ) {
 			$('.owl-carousel-2').owlCarousel({
