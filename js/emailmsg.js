cc44b1534fe664c64efac198851aa96af6be9569
David Cibin <ddcibin@gmail.com>
2024-01-29 11:28:00 -0500

    handle recaptcha response

--- js/emailmsg.js
+++ js/emailmsg.js
@@ -1,6 +1,6 @@
 function msgDiv() {
     if ((window.location.href.split("?", 2)[1]) === "confirm") {
-      setTimeout(function () { document.getElementById("confirmation").style.display = "block"; }, 500);
+      setTimeout(function () { document.getElementById("confirmation").style.display = "flex"; }, 500);
       setTimeout(function () { $(".show-msg").addClass("active"); }, 5000);      
       setTimeout(function () { document.getElementById("confirmation").style.display = "none"; }, 6000);
     }
